206ae34a6f6be57ecaed601a2a531ed2466591eb
Edmilson Torres <edmilson.torres@gmail.com>
2022-04-06 17:17:27 -0300

    chore: routes file

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import router from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/ping', (req: Request, res: Response) => {
-  return res.json({ message: 'pong' });
-});
+app.use(router);
 
 export default app;
